61d6552f94fdb9a19854a3e23e602133d6bf16e2
Ashirwad04 <ashirwadk04@gmail.com>
2024-06-08 13:48:57 +0530

    Completed work related to image

--- src/main/java/Blog/Project/controllers/PostController.java
+++ src/main/java/Blog/Project/controllers/PostController.java
@@ -1,25 +1,37 @@
 package Blog.Project.controllers;
 
 
-
 import Blog.Project.config.AppConstant;
 import Blog.Project.paylods.ApiResponse;
+
 import Blog.Project.paylods.PostDto;
 import Blog.Project.paylods.PostResponse;
+import Blog.Project.services.FileService;
 import Blog.Project.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
 public class PostController {
 
+
+    @Autowired
+    private FileService fileService;
+
     @Autowired
     private PostService postService;
+
+
+    @Value("${project.image}")
+    private String path;
 
     // Create post
     @PostMapping("/user/{userId}/category/{categoryId}/posts")
@@ -53,7 +65,7 @@ public class PostController {
             @RequestParam(value = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER, required = false) Integer pageNumber,
             @RequestParam(value = "pageSize", defaultValue = AppConstant.PAGE_SIZE, required = false) Integer pageSize,
             @RequestParam(value = "shortBy",defaultValue = "postId",required = false)String shortBy
-           ) {
+    ) {
 
         PostResponse postsResponse = this.postService.getAllPosts(pageNumber, pageSize,shortBy);
         return new ResponseEntity<>(postsResponse, HttpStatus.OK);
@@ -97,5 +109,22 @@ public class PostController {
 
     }
 
-}
+    //post image upload
 
+    // Post image upload
+    @PostMapping("/post/image/upload/{postId}")
+    public ResponseEntity<PostDto> uploadImage(
+            @RequestParam("image") MultipartFile image,
+            @PathVariable Integer postId) throws IOException {
+
+        PostDto postDto = this.postService.getPostById(postId);
+        String fileName = this.fileService.uploadImage(path, image);
+
+        postDto.setImageName(fileName);
+        PostDto updatedPost = this.postService.updatePost(postDto, postId);
+        return new ResponseEntity<>(updatedPost, HttpStatus.OK);
+    }
+
+
+
+}
